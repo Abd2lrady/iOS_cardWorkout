552bef3b08a8c4c7e1094c602dac75ff5403c012
Abdelrady <abdelrady@Abdelradys-Mac.local>
2020-10-29 14:53:43 +0200

    app is  working via storyboard

--- iOS_cardWorkout/CardSelectionVC.swift
+++ iOS_cardWorkout/CardSelectionVC.swift
@@ -9,12 +9,52 @@
 import UIKit
 
 class CardSelectionVC: UIViewController {
+    
+    var timer: Timer!
+    var cards = deck.allCards
 
+    @IBOutlet weak var cardImageView: UIImageView!
+    
+    @IBOutlet var buttons: [UIButton]!
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        for button in buttons {
+            button.layer.cornerRadius = 12
+        }
+        startTimer()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        timer.invalidate()
+    }
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.05, target: self, selector: #selector(changeCard), userInfo: nil, repeats: true)
+    }
+    
+    @objc func changeCard() {
+        cardImageView.image = cards.randomElement() ?? UIImage()
+        
+        
+    }
+    @IBAction func stopBtnTapped(_ sender: UIButton) {
+        timer.invalidate()
+    }
+    
+    @IBAction func restartBtnTapped(_ sender: Any) {
+        timer.invalidate()
+        startTimer()
+    }
+    
+   
+    
 }
 
---
+++ iOS_cardWorkout/RulesVC.swift
@@ -0,0 +1,22 @@
+//
+//  RulesVC.swift
+//  iOS_cardWorkout
+//
+//  Created by Abdelrady on 10/29/20.
+//  Copyright © 2020 Abdelrady. All rights reserved.
+//
+
+import UIKit
+
+class RulesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+ 
+    @IBAction func backToPlayBtnTapped(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+}
