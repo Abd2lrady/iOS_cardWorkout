1d5538f87df5b8e2de8724fe98a5bb82b63b976a
Abdelrady <abdelrady@Abdelradys-Mac.local>
2020-10-29 12:09:14 +0200

    main screen layout

---
+++ iOS_cardWorkout/CardSelectionVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_cardWorkout
+//
+//  Created by Abdelrady on 10/29/20.
+//  Copyright © 2020 Abdelrady. All rights reserved.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
